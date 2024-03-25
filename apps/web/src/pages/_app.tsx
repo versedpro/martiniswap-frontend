e7b05e1c31cbc8b753f68ce0e3413bcc25f2f842
versedpro <ultraj0330@gmail.com>
2024-03-26 02:34:06 +0800

    chore: turn app to normal load

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -151,7 +151,7 @@ type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout
 }
 
-const ProductionErrorBoundary = process.env.NODE_ENV === 'production' ? SentryErrorBoundary : Fragment
+const ProductionErrorBoundary = process.env.NODE_ENV !== 'production' ? SentryErrorBoundary : Fragment
 
 const App = ({ Component, pageProps }: AppPropsWithLayout) => {
   if (Component.pure) {
